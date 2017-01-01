e78de960f4c892fad68dfe7d7d21119b3f88e2c5
Cookie Engineer <cookiengineer@protonmail.ch>
2017-01-01 19:06:20 +0100

    :bug: Fixes

--- bin/build.sh
+++ bin/build.sh
@@ -2,19 +2,64 @@
 
 LYCHEEJS_ROOT=$(cd "$(dirname "$(readlink -f "$0")")/../../../"; pwd);
 PROJECT_ROOT=$(cd "$(dirname "$(readlink -f "$0")")/../"; pwd);
-LYCHEEJS_HELPER=`which lycheejs-helper`;
+LYCHEEJS_FERTILIZER=`which lycheejs-fertilizer`;
 
-if [ "$LYCHEEJS_HELPER" == "" ]; then
-	LYCHEEJS_HELPER="$LYCHEEJS_ROOT/bin/helper.sh";
+if [ "$LYCHEEJS_FERTILIZER" == "" ]; then
+	LYCHEEJS_FERTILIZER="$LYCHEEJS_ROOT/bin/fertilizer.sh";
 fi;
 
 
-if [ -e "$LYCHEEJS_HELPER" ]; then
+if [ -e "$LYCHEEJS_FERTILIZER" ]; then
+
+	cd $PROJECT_ROOT;
+
+	rm -rf "$PROJECT_ROOT/release";
+	mkdir -p "$PROJECT_ROOT/release";
+
+
+	$LYCHEEJS_FERTILIZER auto /projects/boilerplate;
+	mv "$LYCHEEJS_ROOT/projects/boilerplate/build/boilerplate_browser_all.zip"    "$PROJECT_ROOT/release/boilerplate_browser_all.zip";
+	mv "$LYCHEEJS_ROOT/projects/boilerplate/build/boilerplate_linux_x86_64.zip"   "$PROJECT_ROOT/release/boilerplate_linux_x86_64.zip";
+	mv "$LYCHEEJS_ROOT/projects/boilerplate/build/boilerplate_osx_x86_64.zip"     "$PROJECT_ROOT/release/boilerplate_osx_x86_64.zip";
+	mv "$LYCHEEJS_ROOT/projects/boilerplate/build/boilerplate_windows_x86_64.zip" "$PROJECT_ROOT/release/boilerplate_windows_x86_64.zip";
+
+	$LYCHEEJS_FERTILIZER auto /projects/lethalmaze;
+	mv "$LYCHEEJS_ROOT/projects/lethalmaze/build/lethalmaze_browser_all.zip"    "$PROJECT_ROOT/release/lethalmaze_browser_all.zip";
+	mv "$LYCHEEJS_ROOT/projects/lethalmaze/build/lethalmaze_linux_x86_64.zip"   "$PROJECT_ROOT/release/lethalmaze_linux_x86_64.zip";
+	mv "$LYCHEEJS_ROOT/projects/lethalmaze/build/lethalmaze_osx_x86_64.zip"     "$PROJECT_ROOT/release/lethalmaze_osx_x86_64.zip";
+	mv "$LYCHEEJS_ROOT/projects/lethalmaze/build/lethalmaze_windows_x86_64.zip" "$PROJECT_ROOT/release/lethalmaze_windows_x86_64.zip";
+
+	$LYCHEEJS_FERTILIZER auto /projects/mode7;
+	mv "$LYCHEEJS_ROOT/projects/mode7/build/mode7_browser_all.zip"    "$PROJECT_ROOT/release/mode7_browser_all.zip";
+	mv "$LYCHEEJS_ROOT/projects/mode7/build/mode7_linux_x86_64.zip"   "$PROJECT_ROOT/release/mode7_linux_x86_64.zip";
+	mv "$LYCHEEJS_ROOT/projects/mode7/build/mode7_osx_x86_64.zip"     "$PROJECT_ROOT/release/mode7_osx_x86_64.zip";
+	mv "$LYCHEEJS_ROOT/projects/mode7/build/mode7_windows_x86_64.zip" "$PROJECT_ROOT/release/mode7_windows_x86_64.zip";
+
+	$LYCHEEJS_FERTILIZER auto /projects/over-there;
+	mv "$LYCHEEJS_ROOT/projects/over-there/build/over-there_browser_all.zip"    "$PROJECT_ROOT/release/over-there_browser_all.zip";
+	mv "$LYCHEEJS_ROOT/projects/over-there/build/over-there_linux_x86_64.zip"   "$PROJECT_ROOT/release/over-there_linux_x86_64.zip";
+	mv "$LYCHEEJS_ROOT/projects/over-there/build/over-there_osx_x86_64.zip"     "$PROJECT_ROOT/release/over-there_osx_x86_64.zip";
+	mv "$LYCHEEJS_ROOT/projects/over-there/build/over-there_windows_x86_64.zip" "$PROJECT_ROOT/release/over-there_windows_x86_64.zip";
+
 
 	cd $PROJECT_ROOT;
 
 	rm -rf "$PROJECT_ROOT/build";
 	mkdir -p "$PROJECT_ROOT/build";
+
+
+	mkdir -p "$PROJECT_ROOT/build/examples/boilerplate";
+	cp -R $LYCHEEJS_ROOT/projects/boilerplate/build/html/main/* $PROJECT_ROOT/build/examples/boilerplate/;
+
+	mkdir -p "$PROJECT_ROOT/build/examples/lethalmaze";
+	cp -R $LYCHEEJS_ROOT/projects/lethalmaze/build/html/main/* $PROJECT_ROOT/build/examples/lethalmaze/;
+
+	mkdir -p "$PROJECT_ROOT/build/examples/mode7";
+	cp -R $LYCHEEJS_ROOT/projects/mode7/build/html/main/* $PROJECT_ROOT/build/examples/mode7/;
+
+	mkdir -p "$PROJECT_ROOT/build/examples/over-there";
+	cp -R $LYCHEEJS_ROOT/projects/over-there/build/html/main/* $PROJECT_ROOT/build/examples/over-there/;
+
 
 	cp "$PROJECT_ROOT/index.html" "$PROJECT_ROOT/build/index.html";
 	cp "$PROJECT_ROOT/examples.html" "$PROJECT_ROOT/build/examples.html";
